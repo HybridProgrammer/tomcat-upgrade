8381d03d17d16c6df06b6667cd4b774cb46364e0
Jason Heithoff <jheithof@fau.edu>
2017-10-03 23:45:51 -0400

    bugfixs and validation checks

--- tomcat-upgrade.sh
+++ tomcat-upgrade.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-mkdir -p staging
-cd staging
-[ -d tomcat ] || git clone https://github.com/HybridProgrammer/tomcat
-cd tomcat
-
-if [ $1 == "list" ] ;  then 
-	git tag
-	exit 0
-fi
-
 VERSION=$1
 cd $2
 DEST=`pwd`
@@ -17,6 +7,16 @@ cd -
 
 [ -z "$VERSION" ] && VERSION="v8.5.20"
 [ -z "$2" ] && DEST=`pwd`
+
+mkdir -p staging
+cd staging
+[ -d tomcat ] || git clone https://github.com/HybridProgrammer/tomcat
+cd tomcat
+
+if [[ $1 == "list" ]] ;  then 
+	git tag
+	exit 0
+fi
 
 echo "Fetching ${VERSION} and upgrading ${DEST}"
 
@@ -27,5 +27,18 @@ if [ $CUR_BRANCH != $VERSION ] ; then
 fi
 #git pull
 
+cp $DEST/conf/* conf/
+cp $DEST/bin/* bin/
+git status | grep modified
+if [[ $? -eq 0 ]] ; then 
+	echo "The following files have been modified by you. Do you want us to merge these file after the upgrade? [n/y]:" 
+	read merge_answer
+
+	if [[ $merge_answer == "n" ]] ; then
+		echo "Stashing modified files"
+		git stash
+	fi
+fi
+
 echo "Upgrading ${DEST}"
-cp -R * $DEST
+cp -r * $DEST
